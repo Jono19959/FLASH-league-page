e9de1d669e452e094736777f997311e2069dd76e
Jono19959 <116454330+Jono19959@users.noreply.github.com>
2022-10-22 11:06:30 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -72,9 +72,9 @@ export const managers = [
       "location": "??????", // (optional)
       "bio": "?????",
       "photo": "/managers/name.png", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": ????, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Rival", // Can be anything (usually your rival's name)
         link: null, // manager array number within this array, or null to link back to all managers page
@@ -138,9 +138,9 @@ export const managers = [
       "location": "??????", // (optional)
       "bio": "?????",
       "photo": "/managers/name.png", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": ????, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": null, // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": 'Win Now', // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Rival", // Can be anything (usually your rival's name)
         link: null, // manager array number within this array, or null to link back to all managers page
