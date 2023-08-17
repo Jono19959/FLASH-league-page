3f45867ae109a92794e8e2797a54b342c212a393
Jono19959 <116454330+Jono19959@users.noreply.github.com>
2023-08-17 09:16:43 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -119,7 +119,7 @@ export const managers = [
       "bio": "2022 Villanova Ultimate FF champ. JoDo's cryptonite...in football and ultimate.",
       "photo": "/managers/everyone.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "nwe", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Jodo...apparently", // Can be anything (usually your rival's name)
